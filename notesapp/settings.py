1b30d1c4f603fe70142ccb1c6f0b651ab6aa5fc4
haseeb-ahad <ha7579654@gmail.com>
2025-01-08 14:30:17 -0800

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -88,11 +88,12 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR 'db.sqlite3')
-        }
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 
 
 # Password validation
